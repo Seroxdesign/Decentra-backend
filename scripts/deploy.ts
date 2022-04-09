9edc59257ec46e9362aaf63e424b763a2791744f
josealonso <jose.alonso.programmer@gmail.com>
2022-04-09 21:54:57 +0200

    Changed the IPFS Id. It points to a Pinata folder now

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -12,7 +12,8 @@ async function main() {
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
   // await hre.run('compile');
-  const baseTokenURI = "ipfs://QmZbWNKJPAjxXuNFSEaksCJVd1M6DaKQViJBYPK2BdpDEP/";
+
+  const baseTokenURI = "ipfs://QmeJmg1pPgiUDYoNZ264g8CkCZjbbgshmWrEWFiyQknEjN/";
 
   // Get owner/deployer's wallet address
   const [owner] = await ethers.getSigners();
