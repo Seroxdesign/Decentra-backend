9459e7ea942c1307c7e64514b4a57fa279ea3b03
josealonso <jose.alonso.programmer@gmail.com>
2022-04-09 16:58:46 +0200

    Deployed locally with "npx hardhat run scripts/deploy.ts --network hardhat"

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -12,19 +12,41 @@ async function main() {
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
   // await hre.run('compile');
+  const baseTokenURI = "ipfs://QmZbWNKJPAjxXuNFSEaksCJVd1M6DaKQViJBYPK2BdpDEP/";
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  // Get owner/deployer's wallet address
+  const [owner] = await ethers.getSigners();
 
-  await greeter.deployed();
+  // Deploy the contract with the correct constructor arguments
+  console.log("================================== SFSG ========================");
+  const contractFactory = await ethers.getContractFactory("DecentraNFT");
+  const NFTContract = await contractFactory.deploy(baseTokenURI);
 
-  console.log("Greeter deployed to:", greeter.address);
+  // Wait for this transaction to be mined
+  await NFTContract.deployed();
+
+  // Get contract address
+  console.log("NFT Contract deployed to:", NFTContract.address);
+
+  // Reserve NFTs
+  let txn = await NFTContract.reserveNFTs();
+  await txn.wait();
+  console.log(await NFTContract.RESERVED_NFTS(), " NFTs have been reserved");
+
+  // Mint 3 NFTs by sending 0.03 ether
+  txn = await NFTContract.mintNFTs(3, { value: ethers.utils.parseEther("0.03") });
+  await txn.wait();
+
+  // Get all tokens IDs of the owner
+  let tokens = await NFTContract.tokensOfOwner(owner.address);
+  console.log("Owner has tokens: ", tokens);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exitCode = 0)
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
