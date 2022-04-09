4f3be9cf75e32cef169cd31f30010affa5668e88
josealonso <jose.alonso.programmer@gmail.com>
2022-04-09 12:48:41 +0200

    Simpler contract, anyone can mint an NFT

--- contracts/CreateNFT.sol
+++ contracts/CreateNFT.sol
@@ -1,90 +1,50 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract CreateNFT is ERC721Enumerable, Ownable {
+contract DecentraNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
-
     Counters.Counter private _tokenIds;
 
-    uint256 public constant MAX_SUPPLY = 100;
-    uint256 public constant PRICE = 0.01 ether;
-    uint256 public constant MAX_PER_MINT = 5;
+    constructor() ERC721("DcentraNFT", "DNFT") {}
 
-    string public baseTokenURI;
-
-    constructor(string memory baseURI) ERC721("Decentra Contract", "NFTC") {
-        setBaseURI(baseURI);
-    }
-
-    function reserveNFTs() public onlyOwner {
-        uint256 totalMinted = _tokenIds.current();
-
-        require(
-            totalMinted + 10 < MAX_SUPPLY,
-            "Not enough NFTs left to reserve"
-        );
-
-        for (uint256 i = 0; i < 3; i++) {
-            _mintSingleNFT();
-        }
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseTokenURI;
-    }
-
-    function setBaseURI(string memory _baseTokenURI) public onlyOwner {
-        baseTokenURI = _baseTokenURI;
-    }
-
-    function mintNFTs(uint256 _count) public payable {
-        uint256 totalMinted = _tokenIds.current();
-
-        require(totalMinted + _count <= MAX_SUPPLY, "Not enough NFTs left!");
-        require(
-            _count > 0 && _count <= MAX_PER_MINT,
-            "Cannot mint specified number of NFTs."
-        );
-        require(
-            msg.value >= PRICE * _count,
-            "Not enough ether to purchase NFTs."
-        );
-
-        for (uint256 i = 0; i < _count; i++) {
-            _mintSingleNFT();
-        }
-    }
-
-    function _mintSingleNFT() private {
-        uint256 newTokenID = _tokenIds.current();
-        _safeMint(msg.sender, newTokenID);
-        _tokenIds.increment();
-    }
-
-    function tokensOfOwner(address _owner)
-        external
-        view
-        returns (uint256[] memory)
+    function mintNFT(address recipient, string memory tokenURI)
+        public
+        returns (uint256)
     {
-        uint256 tokenCount = balanceOf(_owner);
-        uint256[] memory tokensId = new uint256[](tokenCount);
+        _tokenIds.increment();
 
-        for (uint256 i = 0; i < tokenCount; i++) {
-            tokensId[i] = tokenOfOwnerByIndex(_owner, i);
-        }
-        return tokensId;
-    }
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId); // or _safeMint ??
+        _setTokenURI(newItemId, tokenURI);
 
-    function withdraw() public payable onlyOwner {
-        uint256 balance = address(this).balance;
-        require(balance > 0, "No ether left to withdraw");
-
-        (bool success, ) = (msg.sender).call{value: balance}("");
-        require(success, "Transfer failed.");
+        return newItemId;
     }
 }
+
+//     function tokensOfOwner(address _owner)
+//         external
+//         view
+//         returns (uint256[] memory)
+//     {
+//         uint256 tokenCount = balanceOf(_owner);
+//         uint256[] memory tokensId = new uint256[](tokenCount);
+
+//         for (uint256 i = 0; i < tokenCount; i++) {
+//             tokensId[i] = tokenOfOwnerByIndex(_owner, i);
+//         }
+//         return tokensId;
+//     }
+
+//     function withdraw() public payable onlyOwner {
+//         uint256 balance = address(this).balance;
+//         require(balance > 0, "No ether left to withdraw");
+
+//         (bool success, ) = (msg.sender).call{value: balance}("");
+//         require(success, "Transfer failed.");
+//     }
+// }
