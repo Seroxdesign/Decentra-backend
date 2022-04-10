0bdb12e4578672f938d806a29126728b570e390c
josealonso <jose.alonso.programmer@gmail.com>
2022-04-10 12:47:04 +0200

    Set up Alchemy Web3 endpoint and contract

--- frontend/src/utils/interact.js
+++ frontend/src/utils/interact.js
@@ -1,3 +1,11 @@
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+
+const contractABI = require('../contract-abi.json')
+const contractAddress = "0x4C4a07F737Bf57F6632B6CAB089B78f62385aCaE";
+
 export const connectWallet = async () => {
     if (window.ethereum) {
         try {
